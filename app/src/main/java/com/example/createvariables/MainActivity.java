b0cff2231712d923135e4a9bab96be0365647ecf
ofeklevii <ofeklevi@sisma.org.il>
2024-01-02 15:20:20 +0200

    Initial commit

--- app/src/main/java/com/example/createvariables/MainActivity.java
+++ app/src/main/java/com/example/createvariables/MainActivity.java
@@ -10,14 +10,12 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private Button btn;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.button);
     }
-
 
     public void exe1(View view) {
         btn.setText("Oh, yea, I've been clicked!");
