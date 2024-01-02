90d3a4a200ce1f079c24f51df8566d5d9fc20fe1
ofeklevii <ofeklevi@sisma.org.il>
2024-01-02 14:52:35 +0200

    Initial commit

--- app/src/main/java/com/example/createvariables/MainActivity.java
+++ app/src/main/java/com/example/createvariables/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn;
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
